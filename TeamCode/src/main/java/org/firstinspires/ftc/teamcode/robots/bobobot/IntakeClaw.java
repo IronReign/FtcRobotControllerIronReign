eb83fc68004b2c4d7202d7cd1519dfad83848f82
eliasjstewart <144983159+eliasjstewart@users.noreply.github.com>
2023-10-21 00:42:55 -0500

    Bobobot Claw Intake Set Up

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/bobobot/IntakeClaw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/bobobot/IntakeClaw.java
@@ -2,11 +2,22 @@ package org.firstinspires.ftc.teamcode.robots.bobobot;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class IntakeClaw {
-    private Servo clawTilt = null;
+    private Servo clawArm = null;
     private Servo clawSpan = null;
-    //private DcMotorEx slideTilt = null;
-    //private DcMotorEx slideExtension = null;
+    public static double OPENClAW = 0.2;
+    public static double CLOSECLAW = 0.05;
+    public static double armliftSpeed = 0.1;
+    private static int armPosition;
+    private Telemetry telemetry;
+    private HardwareMap hardwareMap;
+
+    public IntakeClaw(Telemetry telemetry, HardwareMap hardwareMap) {
+        this.telemetry = telemetry;
+        this.hardwareMap = hardwareMap;
+    }
+
 
 }
