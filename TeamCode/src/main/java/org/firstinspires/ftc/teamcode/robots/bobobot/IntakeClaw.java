0a9217fd25e98959fb8e714dd816f4ca17fa9089
eliasjstewart <144983159+eliasjstewart@users.noreply.github.com>
2023-10-26 10:46:41 -0500

    Claw and DriveTrain
    Set up code for claw and arm
    Changed direction for wheels on drivetrain

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/bobobot/IntakeClaw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/bobobot/IntakeClaw.java
@@ -1,23 +1,46 @@
 package org.firstinspires.ftc.teamcode.robots.bobobot;
 
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.robots.r2v2.util.Utils;
 
 public class IntakeClaw {
-    private Servo clawArm = null;
+    private DcMotorEx clawArm = null;
     private Servo clawSpan = null;
-    public static double OPENClAW = 0.2;
+    public static double OPENCLAW = 0.2;
     public static double CLOSECLAW = 0.05;
     public static double armliftSpeed = 0.1;
     private static int armPosition;
     private Telemetry telemetry;
     private HardwareMap hardwareMap;
-
     public IntakeClaw(Telemetry telemetry, HardwareMap hardwareMap) {
         this.telemetry = telemetry;
         this.hardwareMap = hardwareMap;
     }
+    public void telemetryOutput()
+    {
+        telemetry.addData("Claw Span  \t", Utils.servoDenormalize(clawSpan.getPosition()));
+        telemetry.addData("Claw Arm Position \t", Utils.servoDenormalize(clawArm.getCurrentPosition()));
+    }
+    public void init()
+    {
+        clawArm = this.hardwareMap.get(DcMotorEx.class, "clawArmServo");
+        clawSpan = this.hardwareMap.get(Servo.class, "clawSpanServo");
+    }
+    public void openClaw () {clawSpan.setPosition(OPENCLAW);}
+    public void closeClaw () {clawSpan.setPosition(CLOSECLAW);}
 
-
+    public void clawArmLift (float position)
+    {
+        armPosition = armPosition + (int)(armliftSpeed*position);
+        if(armPosition < 500)
+            armPosition = 500;
+        if(armPosition < 750)
+            armPosition = 750;
+        clawArm.setPositionPIDFCoefficients(Utils.servoNormalize(armPosition));
+    }
+    public double getClawPosition () { return clawSpan.getPosition();}
 }
+
