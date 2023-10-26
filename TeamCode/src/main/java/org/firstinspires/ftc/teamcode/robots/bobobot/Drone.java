0cd3f69046d24980fab8e5e5ecc53d024068c901
eliasjstewart <144983159+eliasjstewart@users.noreply.github.com>
2023-10-26 17:57:00 -0500

    Drone Class Created
    Created a class and methods for the drone launch

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/bobobot/Drone.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.robots.bobobot;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.robots.r2v2.util.Utils;
+public class Drone {
+    private Servo droneLaunch = null;
+    public static double RELEASE = 0.7;
+    private Telemetry telemetry;
+    private HardwareMap hardwareMap;
+    public Drone(Telemetry telemetry, HardwareMap hardwareMap) {
+        this.telemetry = telemetry;
+        this.hardwareMap = hardwareMap;
+    }
+    public void telemetryOutput()
+    {
+        telemetry.addData("Drone Launch Servo  \t", Utils.servoDenormalize(droneLaunch.getPosition()));
+    }
+    public void init()
+    {
+        droneLaunch = this.hardwareMap.get(Servo.class, "droneLaunchServo");
+    }
+    public void droneRelease () {droneLaunch.setPosition(RELEASE);}
+
+    //public double getDronePosition () { return droneLaunch.getPosition();}
+
+}
+
