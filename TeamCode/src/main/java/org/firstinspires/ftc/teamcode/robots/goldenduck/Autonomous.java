97766e4902791cb5df892937b2b3c429c5ee94b0
Taseen <146591384+Taseen000@users.noreply.github.com>
2023-12-07 10:07:26 -0600

    43res
    rewsd

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/goldenduck/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/goldenduck/Autonomous.java
@@ -1,10 +1,10 @@
-//package org.firstinspires.ftc.teamcode.robots.goldenduck;
-//
-//import com.acmerobotics.dashboard.config.Config;
-//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-//
-//@Config("GoldenDuckGameVariables")
-//@TeleOp(name="Golden Duck OpMode", group="Autonomous")
-//public class Autonomous {
-//
-//}
+package org.firstinspires.ftc.teamcode.robots.goldenduck;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Config("GoldenDuckGameVariables")
+@TeleOp(name="Golden Duck OpMode", group="Autonomous")
+public class Autonomous {
+
+}
