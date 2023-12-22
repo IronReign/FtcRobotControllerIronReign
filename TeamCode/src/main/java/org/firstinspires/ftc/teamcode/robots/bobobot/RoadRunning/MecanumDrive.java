6420d016783fe8261f7aedfb7bbddc746cc7582a
eliasjstewart <144983159+eliasjstewart@users.noreply.github.com>
2023-12-22 15:12:38 -0600

    Package change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/bobobot/RoadRunning/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/bobobot/RoadRunning/MecanumDrive.java
@@ -1,11 +1,13 @@
-package org.firstinspires.ftc.teamcode.robots.csbot.rr_stuff;
+package org.firstinspires.ftc.teamcode.robots.bobobot.RoadRunning;
 
 import androidx.annotation.NonNull;
 
 import com.acmerobotics.dashboard.canvas.Canvas;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-import com.acmerobotics.roadrunner.*;
+import com.acmerobotics.roadrunner.AccelConstraint;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Actions;
 import com.acmerobotics.roadrunner.AngularVelConstraint;
 import com.acmerobotics.roadrunner.DualNum;
 import com.acmerobotics.roadrunner.HolonomicController;
@@ -14,7 +16,10 @@ import com.acmerobotics.roadrunner.MinVelConstraint;
 import com.acmerobotics.roadrunner.MotorFeedforward;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Pose2dDual;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.PoseVelocity2dDual;
 import com.acmerobotics.roadrunner.ProfileAccelConstraint;
+import com.acmerobotics.roadrunner.Rotation2d;
 import com.acmerobotics.roadrunner.Time;
 import com.acmerobotics.roadrunner.TimeTrajectory;
 import com.acmerobotics.roadrunner.TimeTurn;
@@ -40,12 +45,13 @@ import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.robots.csbot.rr_stuff.Localizer;
+import org.firstinspires.ftc.teamcode.robots.csbot.rr_stuff.ThreeDeadWheelLocalizer;
 import org.firstinspires.ftc.teamcode.robots.csbot.rr_stuff.messages.DriveCommandMessage;
 import org.firstinspires.ftc.teamcode.robots.csbot.rr_stuff.messages.MecanumCommandMessage;
 import org.firstinspires.ftc.teamcode.robots.csbot.rr_stuff.messages.MecanumEncodersMessage;
 import org.firstinspires.ftc.teamcode.robots.csbot.rr_stuff.messages.PoseMessage;
 
-import java.lang.Math;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
