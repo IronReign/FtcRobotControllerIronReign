cdd43c55206f4ebbeb617567005ddd0e717604c2
eliasjstewart <144983159+eliasjstewart@users.noreply.github.com>
2024-01-04 01:08:40 -0600

    Autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/bobobot/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/bobobot/Autonomous.java
@@ -5,7 +5,9 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -42,8 +44,16 @@ public class Autonomous extends OpMode {
                 frontRed = new SequentialAction(
                         runnerBot.driveTrain.actionBuilder(runnerBot.driveTrain.pose)
                                 .turn(Math.toRadians(-90))
-                                .lineToY(5)
+                                .lineToX(64)
                         .build()
         );
+
+                frontBlue = new SequentialAction(
+                        runnerBot.driveTrain.actionBuilder(runnerBot.driveTrain.pose)
+                                .turn(Math.toRadians(90))
+                                .lineToX(64)
+                                .build()
+                );
+
     }
 }
