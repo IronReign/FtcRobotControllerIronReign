5d8de71bf78795afef2e51f4ec90c74cec8d2d7a
notjaisammpath <114313475+notjaisammpath@users.noreply.github.com>
2024-10-05 15:52:26 -0500

    new core robot!
    still needs 2 fixes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/core/CoreOpMode.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.robots.core;
+
+import com.acmerobotics.dashboard.canvas.Canvas;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.robotcore.internal.system.Misc;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class CoreOpMode extends OpMode {
+    Robot robot;
+
+    @Override
+    public void init() {
+        robot = new Robot(hardwareMap, gamepad1);
+        robot.init();
+    }
+
+    @Override
+    public void loop() {
+        robot.update(new Canvas());
+        handleTelemetry(robot.getTelemetry(true), robot.getTelemetryName());
+    }
+
+    private void handleTelemetry(Map<String, Object> telemetryMap, String telemetryName) {
+        telemetry.addLine(telemetryName);
+
+        for (Map.Entry<String, Object> entry : telemetryMap.entrySet()) {
+            String line = Misc.formatInvariant("%s: %s", entry.getKey(), entry.getValue());
+            telemetry.addLine(line);
+        }
+
+        telemetry.addLine();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/core/Robot.java
@@ -0,0 +1,94 @@
+package org.firstinspires.ftc.teamcode.robots.core;
+
+import android.content.pm.LabeledIntent;
+
+import com.acmerobotics.dashboard.canvas.Canvas;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.robots.deepthought.subsystem.Subsystem;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Robot implements Subsystem {
+    HardwareMap hardwareMap;
+    DcMotorEx leftFront, leftBack, rightFront, rightBack;
+    Servo claw;
+    Gamepad gamepad1;
+    public boolean clawOpen = false;
+
+    public Robot(HardwareMap hardwareMap, Gamepad gamepad) {
+        this.hardwareMap = hardwareMap;
+        this.gamepad1 = gamepad;
+    }
+
+    @Override
+    public void update(Canvas fieldOverlay) {
+        if(gamepad1.b) {
+            clawOpen = !clawOpen;
+        }
+
+        if(clawOpen) {
+            claw.setPosition(1);
+        } else {
+            claw.setPosition(0);
+        }
+
+        mecanumDrive(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+    }
+
+    public void mecanumDrive(double forward, double strafe, double turn) {
+        double r = Math.hypot(strafe, forward);
+        double robotAngle = Math.atan2(forward, strafe) - Math.PI/4;
+        double rightX = -turn;
+        leftFront.setPower((r * Math.cos(robotAngle) - rightX));
+        rightFront.setPower((r * Math.sin(robotAngle) + rightX));
+        leftBack.setPower((r * Math.sin(robotAngle) - rightX));
+        rightBack.setPower((r * Math.cos(robotAngle) + rightX));
+    }
+
+
+    @Override
+    public void stop() {
+
+    }
+
+    public void init() {
+        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
+        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
+        claw = hardwareMap.get(Servo.class, "claw");
+        // Set motor runmodes
+        leftBack.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        rightBack.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        leftFront.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftFront.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        leftBack.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        rightFront.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        rightBack.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    @Override
+    public Map<String, Object> getTelemetry(boolean debug) {
+        LinkedHashMap<String, Object> telemetry = new LinkedHashMap<>();
+
+        telemetry.put("Claw Open", clawOpen);
+        telemetry.put("hi", "mom");
+
+        return telemetry;
+    }
+
+    @Override
+    public String getTelemetryName() {
+        return "Core Robot";
+    }
+
+}
