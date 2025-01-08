8a89442b52bfd6578115f850717ef398383da2f8
F3r <fer142@icloud.com>
2025-01-07 19:26:38 -0600

    Speciminer colour sensing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/deepthought/subsystem/samplers/RollerClaw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/deepthought/subsystem/samplers/RollerClaw.java
@@ -1,11 +1,17 @@
 package org.firstinspires.ftc.teamcode.robots.deepthought.subsystem.samplers;
 
+import android.graphics.Color;
+
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.robots.deepthought.subsystem.Subsystem;
+
 import java.util.Map;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.CRServoImplEx;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
@@ -16,67 +22,231 @@ import java.util.Map;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import android.graphics.Color;
+import org.firstinspires.ftc.robotcore.internal.system.Misc;
+
 import java.util.*;
+
 import org.firstinspires.ftc.teamcode.robots.deepthought.util.Utils;
 
-
-
-
-
+import org.firstinspires.ftc.teamcode.robots.deepthought.util.Constants;
 //import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
 //import com.qualcomm.robotcore.hardware.CRServo;
 //import com.qualcomm.robotcore.hardware.CRServoImplEx;
-@Config()
-public class RollerClaw {
-    HardwareMap hardwareMap;
-    Gamepad gamepad1;
-    Telemetry telemetry;
-    CRServo servo1;
-    CRServo servo2;
+import static org.firstinspires.ftc.teamcode.util.utilMethods.futureTime;
+import static org.firstinspires.ftc.teamcode.util.utilMethods.isPast;
+
+
+@TeleOp
+
+public class RollerClaw extends OpMode {
+    public CRServo CRSOne;
+    public CRServo CRSTwo;
+    public DcMotor motor;
+    public NormalizedColorSensor colorSensor;
+
+
     boolean INTAKE;
-    boolean OUTAKE;
+    boolean EJECT;
+    boolean colSensorStat;
+
+
+    public Constants.Alliance alliance = Constants.Alliance.RED;
+    String allianceCol = "RED";
+    String appoAllianceCol = "BLUE";
 
     public void init() {
-        servo1 = hardwareMap.get(CRServo.class,"servo1");
-        servo2 = hardwareMap.get(CRServo.class,"servo2");
+        CRSOne = this.hardwareMap.get(CRServo.class, "CRSOne");
+        CRSTwo = this.hardwareMap.get(CRServo.class, "CRSTwo");
+        colorSensor = this.hardwareMap.get(NormalizedColorSensor.class, "colorSensor");
+
         INTAKE = false;
-        OUTAKE = false;
-
+        EJECT = false;
+        colSensorStat = false;
     }
-
+// 90,60,120,150 - no sample
+//    200-215 - blue
+    // 0-15 - red
+    // 50-59 - no sample
     public void loop() {
-        if(INTAKE) {
-            servo1.setPower(-1);
-            servo2.setPower(1);
+        if (INTAKE) {
+            if(!sampleDetected()) {
+                CRSOne.setPower(-1);
+                CRSTwo.setPower(1);
+                colSensorStat = true;
+            }
+            else if(apposingSample()) {
+                INTAKE = false;
+                EJECT = true;
+            }
+            else{
+                CRSOne.setPower(0);
+                CRSTwo.setPower(0);
+            }
         }
-        else if(OUTAKE) {
-            servo1.setPower(1);
-            servo2.setPower(-1);
-        }
-        else
-        {
-            servo1.setPower(0);
-            servo2.setPower(0);
+        double ejectTimer;
+        ejectTimer = futureTime(.5);
+        if (EJECT) {
+
+                if (sampleDetected()) {
+                    CRSOne.setPower(1);
+                    CRSTwo.setPower(-1);
+                    colSensorStat = true;
+
+//                     if(!sampleDetected()){
+//                        CRSOne.setPower(-1);
+//                        CRSTwo.setPower(1);
+//                        colSensorStat = true;
+//                    }
+                }
+                else if(!sampleDetected()) {
+                    if(CRSOne.getPower() == 1 && CRSTwo.getPower() == -1){
+                        EJECT = false;
+                        INTAKE = true;
+                        colSensorStat = true;
+                    }
+                    else{
+                        EJECT = false;
+                        INTAKE = false;
+                        colSensorStat = false;
+                    }
+                }
+//            telemetry.addData("ejectTimer", ejectTimer);
+            }
+
+        if (!INTAKE && !EJECT) {
+            CRSOne.setPower(0);
+            CRSTwo.setPower(0);
+            colSensorStat = false;
         }
 
 
 
-        if(gamepad1.a) {
-            INTAKE = false;
-            OUTAKE = false;
-        }
-        if(gamepad1.b){
+
+        if (gamepad1.y) {
             INTAKE = true;
-            OUTAKE = false;
+            EJECT = false;
+        }
+        if (gamepad1.b) {
+            EJECT = true;
+            INTAKE = false;
+        }
+        if (gamepad1.a) {
+            INTAKE = false;
+            EJECT = false;
         }
         if(gamepad1.x){
-            OUTAKE = true;
-            INTAKE = false;
+            if(allianceCol.equals("RED")){
+                alliance = Constants.Alliance.BLUE;
+                allianceCol = "BLUE";
+                appoAllianceCol = "RED";
+            }
+            else{
+                alliance = Constants.Alliance.RED;
+                allianceCol = "RED";
+                appoAllianceCol = "BLUE";
+            }
         }
 
+//        if(INTAKE) {
+////            if (stopOnSample()) {
+////               // colSensorStat = false;
+////                INTAKE = false;
+////                EJECT = false;
+////            }
+////        if(noSample()){
+////            EJECT = false;
+////            //INTAKE = true;
+////        }
+////            if (apposingSample()) {
+////                //colSensorStat = false;
+////                INTAKE = false;
+////                EJECT = true;
+////            }
+//        }
+
+
+
+//        if (gamepad1.a) {
+//            INTAKE = false;
+//            EJECT = false;
+//            colSensorStat = false;
+//        }
+//        if(stopOnSample()){
+//            colSensorStat = false;
+//            INTAKE = false;
+//            //OUTAKE = false;
+//        }
+
+
+
+
+        telemetry.update();
         telemetry.addData("in", INTAKE);
-        telemetry.addData("out", OUTAKE);
+        telemetry.addData("out", EJECT);
+        telemetry.addData("power1", CRSOne.getPower());
+        telemetry.addData("power2", CRSTwo.getPower());
+        telemetry.addData("colorSensorStatus", colSensorStat);
+        telemetry.addData("Sample", colSensorView());
+        telemetry.addData("colorSensorHSV", getHSV()[0]);
+        telemetry.addData("Alliance", allianceCol);
+        telemetry.addData("ejectTimer", ejectTimer);
+
 
     }
+
+    public boolean stopOnSample(){
+        if(colSensorView().equals(allianceCol) || colSensorView().equals("neutral")){
+
+            return true;
+        }
+        return false;
+    }
+
+    public boolean apposingSample(){
+        if(colSensorView().equals(appoAllianceCol)){
+            return true;
+        }
+        return false;
+    }
+    public boolean sampleDetected(){
+        if(colSensorView().equals("noSample")){
+            return false;
+        }
+        return true;
+    }
+
+    public float[] getHSV(){
+        float[] hsv = new float[3];
+        Color.colorToHSV(colorSensor.getNormalizedColors().toColor(),hsv);
+        return hsv;
+    }
+//
+    public String colSensorView(){
+        double hue = getHSV()[0];
+        if(hue < 59 && hue > 40){
+            return "neutral";
+        }
+        else if(hue < 15 && hue > 1){
+            return "RED";
+        }
+        else if(hue < 215 && hue > 200){
+            return "BLUE";
+        }
+        else{
+            return "noSample";
+        }
+   }
+    // 90,60,120,150 - no sample
+//    200-215 - blue
+    // 0-15 - red
+    // 50-59 - no sample
+
+
+
+
+
 }
