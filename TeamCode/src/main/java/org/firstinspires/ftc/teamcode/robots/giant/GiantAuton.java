3010923cdf3c7fd07ee0cc374603dd121cd9f84a
ironreign <ironreignrobotics@gmail.com>
2025-01-16 22:04:43 -0600

    Update GiantAuton.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/giant/GiantAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/giant/GiantAuton.java
@@ -11,10 +11,6 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-
-
-
-import org.checkerframework.checker.units.qual.A;
 import org.firstinspires.ftc.robotcore.internal.system.Misc;
 
 
