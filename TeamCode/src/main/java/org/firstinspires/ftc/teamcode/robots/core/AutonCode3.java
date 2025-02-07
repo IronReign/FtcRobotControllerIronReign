153289314c048f9dffc1bf419a4e3715a77f3330
ironreign <ironreignrobotics@gmail.com>
2025-02-07 12:15:06 -0600

    set up pid controllers and reorganize robot class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/core/AutonCode3.java
@@ -0,0 +1,347 @@
+package org.firstinspires.ftc.teamcode.robots.core;
+
+import static org.firstinspires.ftc.teamcode.util.utilMethods.futureTime;
+import static org.firstinspires.ftc.teamcode.util.utilMethods.isPast;
+import com.qualcomm.robotcore.hardware.PIDCoefficients;
+import org.firstinspires.ftc.teamcode.util.PIDController;
+
+import com.acmerobotics.dashboard.canvas.Canvas;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.internal.system.Misc;
+import org.firstinspires.ftc.teamcode.robots.csbot.util.StickyGamepad;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Autonomous(name = "COREAUTON3")
+public class AutonCode3 extends OpMode {
+    Robot robot;
+    BNO055IMU imu;
+    private FtcDashboard dashboard;
+    boolean runAuton = true;
+    int autonIndex = 0;
+    int startpos = 0;
+    public double wheelCircum = ((3.5)*Math.PI);
+    public int ticksrev = 1440;
+    boolean moving = false;
+    boolean turning = false;
+    public int targetTicks = 0;
+    boolean vertical = true;
+    boolean horizontal = false;
+    double distance = 0;
+    double target = 0;
+    double initialzOrientation = 0;
+    double nowOrientation = 0;
+    public long autonTimer = 0;
+    public StickyGamepad spad1;
+    boolean reached = false;
+
+    @Override
+    public void init() {
+        spad1 = new StickyGamepad(gamepad1);
+        dashboard = FtcDashboard.getInstance();
+        robot = new Robot(hardwareMap, null);
+
+    }
+
+    public void init_loop(){
+        debug(new Canvas());
+
+    }
+
+
+    public void forward(double length, double direction){
+        if (!moving){
+            // Number of encoder ticks per distance
+            targetTicks = (int)((length/wheelCircum)*ticksrev);
+
+            // Assign initial encoder values
+            startpos = robot.vertical.getCurrentPosition();
+
+            // Indicate Vertical/Horizontal
+            vertical = true;
+            horizontal = false;
+
+            // Travel Distance
+            robot.mecanumDrive(-direction,0,0);
+
+            // Update moving
+            moving = true;
+        }
+    }
+
+    public void strafe(double length, double direction){
+        if (!moving){
+            // Number of encoder ticks per distance
+            targetTicks = (int)((length/wheelCircum)*ticksrev);
+
+            // Assign initial encoder values
+            startpos = robot.horizontal.getCurrentPosition();
+
+            // Indicate Vertical/Horizontal
+            vertical = false;
+            horizontal = true;
+
+            // Travel Distance
+            robot.mecanumDrive(0, direction,0);
+
+            // Update moving
+            moving = true;
+        }
+    }
+
+    public void debug(Canvas fieldOverlay){
+        spad1.update();
+        if(spad1.a){
+            autonIndex++;
+        }
+        if(spad1.x){
+            autonIndex--;
+        }
+        handleTelemetry(getTelemetry(true), robot.getTelemetryName());
+    }
+
+    public boolean completed(){
+        if (moving) {
+            if (vertical){
+                distance = robot.vertical.getCurrentPosition()-startpos;
+            }
+
+            else if (horizontal){
+                distance = robot.horizontal.getCurrentPosition()-startpos;
+            }
+
+            if (Math.abs(distance) >= Math.abs(targetTicks)) {
+                robot.mecanumDrive(0, 0, 0);
+                vertical = false;
+                horizontal = false;
+                moving = false;
+                return true;
+
+            }
+        }
+        return false;
+    }
+
+    public boolean execute(){
+        switch(autonIndex) {
+            // Starting Position: A3 facing submersible with specimen in hand
+            // Specimen one
+            case 0:
+                //literally everything setup
+                //TODO: shoulder adjust; hunch its too low
+                //TODO: timing
+                //5:30-7:30
+                robot.claw.setPosition(robot.clawClosePosition);
+                forward(69, 0.04); //OG: 60
+                robot.shoulder.setTargetPosition(1785);//OG: 275, 220+1647=1867
+                robot.slide.setTargetPosition(350); //444
+                if (completed()) {
+                    autonIndex++;
+                    robot.mecanumDrive(0, 0, 0);
+                    autonTimer = futureTime(1);
+                }
+                break;
+
+            case 1:
+                //secure pres
+                if (isPast(autonTimer)) {
+                    robot.shoulder.setTargetPosition(1360);
+                    if (robot.shoulder.getCurrentPosition() <= robot.shoulder.getTargetPosition()) {
+                        autonIndex++;
+                        autonTimer = futureTime(1);
+                    }
+                }
+                break;
+
+            case 2:
+                //open claw
+                if (isPast(autonTimer)) {
+                    robot.claw.setPosition(robot.clawOpenPosition);
+                    autonIndex++;
+                }
+                break;
+
+            case 3:
+                //Back up
+                forward(50, -0.04); //OG: 60
+                if (completed()) {
+                    autonIndex++;
+                    robot.mecanumDrive(0, 0, 0);
+                }
+
+                break;
+
+            // Specimen dos
+            //secure the president
+
+            case 4:
+                //turn 180
+                //TODO: pid works?
+                robot.turnUntilDegreesIMU(180, 1);
+                if (completed()) {
+                    autonIndex++;
+                    robot.mecanumDrive(0, 0, 0);
+                }
+
+                break;
+
+            case 5:
+                strafe(35, -1);
+                if (completed()) {
+                    autonIndex++;
+                    robot.mecanumDrive(0,0,0);
+                }
+
+            case 6:
+                forward(19, 0.04);
+                if (completed()) {
+                    autonIndex++;
+                    robot.mecanumDrive(0,0,0);
+                }
+
+            case 7:
+                //close
+                robot.claw.setPosition(robot.clawClosePosition);
+                //TODO: edit here based on 1
+                robot.shoulder.setTargetPosition(1785);
+                forward(19, -0.04);
+                if (completed()) {
+                    autonIndex++;
+                    robot.mecanumDrive(0,0,0);
+                }
+                break;
+
+            //back to submersible
+            case 8:
+                robot.turnUntilDegreesIMU(180, 1);
+                if (completed()) {
+                    autonIndex++;
+                    robot.mecanumDrive(0, 0, 0);
+                }
+
+            case 9:
+                strafe(35, -1);
+                if (completed()) {
+                    autonIndex++;
+                    robot.mecanumDrive(0,0,0);
+                }
+
+                //attach on high
+                //TODO: edit based on 1
+            case 10:
+                forward(50, 0.04); //OG: 60
+                robot.shoulder.setTargetPosition(1785);//OG: 275, 220+1647=1867
+                robot.slide.setTargetPosition(350); //444
+                if (completed()) {
+                    autonIndex++;
+                    robot.mecanumDrive(0, 0, 0);
+                    autonTimer = futureTime(1);
+                }
+                break;
+
+            case 11:
+                //secure specimen
+                if (isPast(autonTimer)) {
+                    robot.shoulder.setTargetPosition(1360);
+                    if (robot.shoulder.getCurrentPosition() <= robot.shoulder.getTargetPosition()) {
+                        autonIndex++;
+                        autonTimer = futureTime(1);
+                    }
+                }
+                break;
+
+            case 12:
+                //open claw
+                if (isPast(autonTimer)) {
+                    robot.claw.setPosition(robot.clawOpenPosition);
+                    autonIndex++;
+                }
+                break;
+
+            case 13:
+                //Back up
+                forward(50, -0.04); //OG: 60
+                robot.slide.setTargetPosition(0);
+                if (completed()) {
+                    autonIndex++;
+                    robot.mecanumDrive(0, 0, 0);
+                }
+
+                // Park
+            case 14:
+                strafe(35, -1);
+                robot.slide.setTargetPosition(0);
+                if(completed()){
+                    autonIndex++;
+                    robot.mecanumDrive(0,0,0);
+                    autonIndex = 0;
+                    return true;
+                }
+
+            default:
+                break;
+        }
+        return false;
+    }
+
+    @Override
+    public void loop() {
+        debug(new Canvas());
+        if (runAuton) {
+            runAuton = !execute();
+        }
+    }
+
+    private void handleTelemetry(Map<String, Object> telemetryMap, String telemetryName) {
+        TelemetryPacket p = new TelemetryPacket();
+        telemetry.addLine(telemetryName);
+
+        for (Map.Entry<String, Object> entry : telemetryMap.entrySet()) {
+            String line = Misc.formatInvariant("%s: %s", entry.getKey(), entry.getValue());
+            telemetry.addLine(line);
+            p.addLine(line);
+        }
+        telemetry.addLine();
+        dashboard.sendTelemetryPacket(p);
+    }
+
+    public Map<String, Object> getTelemetry(boolean debug) {
+        LinkedHashMap<String, Object> telemetry = new LinkedHashMap<>();
+
+        telemetry.put("Claw Open", robot.clawOpen);
+        telemetry.put("Shoulder Power", robot.shoulder.getCurrent(CurrentUnit.AMPS));
+        telemetry.put("Shoulder Position", robot.shoulder.getCurrentPosition());
+        telemetry.put("Shoulder Target Position", robot.shoulder.getTargetPosition());
+        telemetry.put("Shoulder runMode", robot.shoulder.getMode());
+
+        telemetry.put("Power", robot.leftFront.getPower());
+        telemetry.put("Target Ticks", targetTicks);
+        telemetry.put("Moving", moving);
+        telemetry.put("Distance", distance);
+        telemetry.put("Reached", reached);
+        telemetry.put("Degrees", robot.getZorient()); // you want to comment this out - getting the imu heading is expensive - get only when needed
+
+        telemetry.put("Slide Position", robot.slide.getCurrentPosition());
+        telemetry.put("Slide Target Position", robot.slideTargetPosition);
+        telemetry.put("Horizontal", robot.horizontal.getCurrentPosition());
+        telemetry.put("Vertical", robot.vertical.getCurrentPosition());
+        telemetry.put("auton Index", autonIndex);
+
+        return telemetry;
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/core/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/core/Robot.java
@@ -1,21 +1,31 @@
 package org.firstinspires.ftc.teamcode.robots.core;
 
+import static org.firstinspires.ftc.teamcode.robots.deepthought.util.Utils.wrapAngle;
 import static org.firstinspires.ftc.teamcode.util.utilMethods.futureTime;
 import static org.firstinspires.ftc.teamcode.util.utilMethods.isPast;
 
 import com.acmerobotics.dashboard.canvas.Canvas;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.PIDCoefficients;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.teamcode.robots.csbot.util.StickyGamepad;
 import org.firstinspires.ftc.teamcode.robots.deepthought.subsystem.Subsystem;
+import org.firstinspires.ftc.teamcode.util.PIDController;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -30,6 +40,10 @@ public class Robot implements Subsystem {
     DcMotorEx slide;
     public static final double CURRENT_THRESHOLD = 2.2;
     public static int CALIBRATE_POSITION = Integer.MAX_VALUE;
+    long autonTimer = 0;
+
+    long totalRunTime=0;
+    long startTime;
 
     public boolean clawOpen = false;
     public double clawOpenPosition = 1;
@@ -39,11 +53,109 @@ public class Robot implements Subsystem {
     public int rotaterPosition = 0;
     private boolean motorUpdated;
     public StickyGamepad spad1;
+    
+    //pid stuff
+    BNO055IMU imu;
+    public static PIDController headingPID;
+    public static PIDCoefficients HEADING_PID_PWR = new PIDCoefficients(0.03, 0.04, 0);
+    public static double HEADING_PID_TOLERANCE = .08;           //.08 //this is a percentage of the input range .063 of 2PI is 1 degree
+    private double PIDCorrection, PIDError, targetHeading, targetDistance;
+    boolean imuTurnDone = false;
+
+    public DistanceSensor sensorDistance;
+    public static PIDController distancePID;
+    public static PIDCoefficients DISTANCE_PID_PWR = new PIDCoefficients(0.03, 0.04, 0);
+    public static double DISTANCE_PID_TOLERANCE = .01; //this is a percentage of the input range .063 of 2PI is 1 degree
+    private double distPIDCorrection, distPIDError;
+    boolean distDriveDone;
+
+    public static PIDController travelPID; //straight line travel using odopods
+    public static PIDCoefficients TRAVEL_PID_PWR = new PIDCoefficients(0.03, 0.04, 0);
+    public static double TRAVEL_PID_TOLERANCE = .01; //this is a percentage of the input range .063 of 2PI is 1 degree
+    private double travelPIDCorrection, travelPIDError;
+    boolean travelDriveDone;
+    
+    
 
     public Robot(HardwareMap hardwareMap, Gamepad gamepad) {
         this.hardwareMap = hardwareMap;
         this.gamepad1 = gamepad;
         spad1 = new StickyGamepad(gamepad1);
+
+        // Call motors from hardwareMap
+        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
+        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
+        shoulder = hardwareMap.get(DcMotorEx.class, "shoulder");
+        claw = hardwareMap.get(Servo.class, "claw");
+        slide = hardwareMap.get(DcMotorEx.class, "slide");
+        vertical = hardwareMap.get(DcMotorEx.class, "vertical");
+        horizontal = hardwareMap.get(DcMotorEx.class, "horizontal");
+
+        // Restart motors
+        leftBack.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightBack.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        leftFront.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightFront.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        shoulder.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        slide.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        vertical.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        horizontal.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+
+        shoulder.setPower(1);
+        shoulder.setVelocity(50);
+        shoulder.setTargetPosition(1500);
+        shoulder.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+
+        slide.setPower(1);
+        slide.setVelocity(50);
+        slide.setTargetPosition(0);
+        slide.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+
+        leftFront.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        horizontal.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        vertical.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+
+        claw.setPosition(clawClosePosition);
+
+        initIMU();
+
+        sensorDistance = hardwareMap.get(DistanceSensor.class, "sensorDistance");
+        
+        // init PID controllers
+        headingPID = new PIDController(HEADING_PID_PWR); // for turning
+        headingPID.setInputRange(0, 360);
+        headingPID.setOutputRange(-1, 1);
+        headingPID.setIntegralCutIn(4);
+        headingPID.setContinuous(true);
+        headingPID.setTolerance(HEADING_PID_TOLERANCE);
+        headingPID.enable();
+
+        distancePID = new PIDController(DISTANCE_PID_PWR); // for distance sensor
+        distancePID.setInputRange(0, 200); //2 meter distance sensor
+        distancePID.setOutputRange(-1, 1);
+        distancePID.setIntegralCutIn(3);
+        distancePID.setContinuous(false);
+        distancePID.setTolerance(DISTANCE_PID_TOLERANCE);
+        distancePID.enable();
+
+        travelPID = new PIDController(TRAVEL_PID_PWR); // for travel sensor
+        travelPID.setInputRange(0, 200); //2 meter travel sensor
+        travelPID.setOutputRange(-1, 1);
+        travelPID.setIntegralCutIn(3);
+        travelPID.setContinuous(false);
+        travelPID.setTolerance(TRAVEL_PID_TOLERANCE);
+        travelPID.enable();
+        
+        
+        totalRunTime=0;
+        startTime = System.currentTimeMillis();
     }
 
     @Override
@@ -65,7 +177,7 @@ public class Robot implements Subsystem {
 
         if(spad1.x) { //preset for wall
             shoulderTargetPosition= 824;
-            slideTargetPosition = 0;
+            slideTargetPosition = 30;
         }
         if(spad1.y) { //preset for specimen score
             shoulderTargetPosition = 1900;
@@ -75,6 +187,29 @@ public class Robot implements Subsystem {
             claw.setPosition(clawOpenPosition);
         } else {
             claw.setPosition(clawClosePosition);
+        }
+
+        //TODO: driver testing (8:00-9:30)
+
+        // Attaching the specimen on the high bar preset (test)
+        if(spad1.left_bumper){
+            shoulderTargetPosition=1800;
+            slideTargetPosition=350;
+            autonTimer = futureTime(1);
+            if(isPast(autonTimer)) {
+                shoulderTargetPosition=1360;
+            }
+        }
+
+        // Attaching picking up the specimen from the wall (test)
+        if(spad1.right_bumper){
+            shoulderTargetPosition=824;
+            slideTargetPosition=30;
+            autonTimer=futureTime(1);
+            if(isPast(autonTimer)){
+                claw.setPosition(clawClosePosition);
+                shoulderTargetPosition=1800;
+            }
         }
 
         if(gamepad1.left_trigger >= 0.3){
@@ -110,6 +245,21 @@ public class Robot implements Subsystem {
 
         updateMotors();
         mecanumDrive(gamepad1.left_stick_y, gamepad1.right_stick_x, gamepad1.left_stick_x);
+    }
+
+    public float getZorient(){
+        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZXY, AngleUnit.DEGREES);
+        return angles.firstAngle;
+    }
+    public void initIMU(){
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.mode = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+
+        imu.initialize(parameters);
     }
 
     public void updateMotors() {
@@ -185,6 +335,53 @@ public class Robot implements Subsystem {
         vertical.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
 
         claw.setPosition(clawOpenPosition);
+    }
+
+    //request a turn in degrees units
+    //this is an absolute (non-relative) implementation.
+    //the direction of the turn will favor the shortest approach
+    public boolean turnUntilDegreesIMU(double turnAngle, double maxSpeed) {
+        targetHeading = wrapAngle(turnAngle);
+        headingPID.setPID(HEADING_PID_PWR);
+        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        headingPID.setInput(angles.firstAngle); // todo - is this yaw?
+        headingPID.setSetpoint(targetHeading);
+        headingPID.setOutputRange(-maxSpeed, maxSpeed);
+        headingPID.setTolerance(HEADING_PID_TOLERANCE);
+        double correction = headingPID.performPID();
+        PIDCorrection = correction;
+        PIDError = headingPID.getError();
+        if (headingPID.onTarget()) {
+            //turn meets accuracy requirement
+            mecanumDrive(0,0,0);
+            return imuTurnDone = true;
+        } else {
+            headingPID.enable();
+            mecanumDrive(0,0,correction);
+            return imuTurnDone = false;
+        }
+    }
+
+    // pid to drive to a target distance sensor value
+    public boolean driveDistance(double distance, double maxSpeed) {
+        targetDistance = wrapAngle(distance);
+        distancePID.setPID(DISTANCE_PID_PWR);
+        distancePID.setInput(sensorDistance.getDistance(DistanceUnit.CM));  // distance reported by distance sensor
+        distancePID.setSetpoint(targetDistance);
+        distancePID.setOutputRange(-maxSpeed, maxSpeed);
+        distancePID.setTolerance(DISTANCE_PID_TOLERANCE);
+        double correction = distancePID.performPID();
+        distPIDCorrection = correction;
+        distPIDError = distancePID.getError();
+        if (distancePID.onTarget()) {
+            //distance meets accuracy requirement
+            mecanumDrive(0,0,0);
+            return distDriveDone = true;
+        } else {
+            distancePID.enable();
+            mecanumDrive(-correction,0,0);
+            return distDriveDone = false;
+        }
     }
 
     @Override
