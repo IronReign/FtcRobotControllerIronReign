ece8e924705a6fd77ad7c03d8da700b27f8be0bf
notjaisammpath <114313475+notjaisammpath@users.noreply.github.com>
2025-02-21 21:41:02 -0600

    btruh

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/deepthought/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/deepthought/Autonomous.java
@@ -24,7 +24,7 @@ import java.util.Map;
 
 @Config(value = "AA_ITD_Auto_Basket")
 public class Autonomous implements TelemetryProvider {
-    public static double AUTON_OUTTAKE_WAIT_TIMER = 1;
+    public static double AUTON_OUTTAKE_WAIT_TIMER = 1.75;
     public static int numCycles = 4;
     private Robot robot;
     private HardwareMap hardwareMap;
