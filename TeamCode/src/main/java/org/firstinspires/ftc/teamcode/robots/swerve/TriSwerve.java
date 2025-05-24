313b6d555c910dd63e3178b48c23f9697b2d36e1
F3r <fer142@icloud.com>
2025-05-24 15:02:15 -0500

    quicktrip changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/swerve/TriSwerve.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/swerve/TriSwerve.java
@@ -1,13 +1,23 @@
 package org.firstinspires.ftc.teamcode.robots.swerve;
 
+import static org.firstinspires.ftc.teamcode.util.utilMethods.futureTime;
+import static org.firstinspires.ftc.teamcode.util.utilMethods.isPast;
+import static org.firstinspires.ftc.teamcode.util.utilMethods.withinError;
+
 import com.acmerobotics.dashboard.canvas.Canvas;
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.PIDCoefficients;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
@@ -15,15 +25,23 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.teamcode.robots.deepthought.subsystem.Subsystem;
 import org.firstinspires.ftc.teamcode.robots.deepthought.util.Utils;
+import org.firstinspires.ftc.teamcode.robots.swervolicious.rr_localize.PinpointLocalizer;
+import org.firstinspires.ftc.teamcode.rrQuickStart.Localizer;
 import org.firstinspires.ftc.teamcode.util.PIDController;
 
 import java.util.Map;
 
-@Config(value = "Swerve")
+@Config(value = "TriSwerve")
 public class TriSwerve implements Subsystem {
+    public static int quickTripDistance = 215;
+    public static int fourCornersDistance = 40;
+    public static double IN_PER_TICK = 0.00293063133;
 
     HardwareMap hardwareMap;
+    Limelight3A limelight;
     SwerveModule swerveModule1, swerveModule2, swerveModule3;
+    Servo gripper;
+    Localizer localizer;
 
     BNO055IMU imu;
     Orientation angles = new Orientation();
@@ -35,10 +53,15 @@ public class TriSwerve implements Subsystem {
     public static double ticksPerDegree = 4920.0 / 360;
     // Set a threshold (in degrees) below which the drive motor is allowed to run.
     public static double yawThreshold = 10;
+    public static boolean gripperOpen = true;
+    public static int gripperOpenPosition = 1500;
+    public static int gripperClosedPosition = 1100;
+    public static int gripperStartPosition = 899;
+    public boolean gripperReady = false;
 
     public TriSwerve(HardwareMap hardwareMap) {
         this.hardwareMap = hardwareMap;
-
+        localizer = new PinpointLocalizer(hardwareMap, IN_PER_TICK, new Pose2d(new Vector2d(0, 0), 0));
         // Get hardware devices for the swerve module.
         DcMotorEx driveMotor1 = hardwareMap.get(DcMotorEx.class, "go1");
         CRServo yawServo1 = hardwareMap.get(CRServo.class, "yaw1");
@@ -51,6 +74,9 @@ public class TriSwerve implements Subsystem {
         DcMotorEx driveMotor3 = hardwareMap.get(DcMotorEx.class, "go3");
         CRServo yawServo3 = hardwareMap.get(CRServo.class, "yaw3");
         DcMotorEx yawEncoder3 = hardwareMap.get(DcMotorEx.class, "encoder3");
+
+        gripper = hardwareMap.get(Servo.class, "gripper");
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
 
         // Reset encoders and configure the drive motor.
         yawEncoder1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -99,6 +125,29 @@ public class TriSwerve implements Subsystem {
         initIMU();
     }
 
+    public void rotate(double turnPower) {
+
+        // Where each module sits, measured CCW from the robot’s +Y (forward) axis
+        // -- adjust these three numbers if your frame layout is different.
+        final double[] MODULE_BEARINGS = {0, 240, 120};   // deg
+
+        // Tangential direction = bearing ±90 deg
+        double tangentOffset = turnPower >= 0 ? 90 : -90;
+        double speed = Math.abs(turnPower);                 // 0-1 drive power
+
+        // Module 1
+        double tgt1 = Utils.wrapAngle(MODULE_BEARINGS[0] + tangentOffset);
+        swerveModule1.setDesiredState(tgt1, speed);
+
+        // Module 2
+        double tgt2 = Utils.wrapAngle(MODULE_BEARINGS[1] + tangentOffset);
+        swerveModule2.setDesiredState(tgt2, speed);
+
+        // Module 3
+        double tgt3 = Utils.wrapAngle(MODULE_BEARINGS[2] + tangentOffset);
+        swerveModule3.setDesiredState(tgt3, speed);
+    }
+
     /**
      * This method converts driver inputs (from the left joystick) into a desired velocity:
      * - The direction (theta) is calculated from the x and y deflections.
@@ -116,7 +165,7 @@ public class TriSwerve implements Subsystem {
         if (deflection > 0.2) {  // Only update desired angle if joystick deflection is significant.
             // Compute the desired angle from joystick input and add chassisHeading for field orientation.
             desiredAngle = Utils.wrapAngle(Math.toDegrees(Math.atan2(joystickX, joystickY)) + chassisHeading);
-            speed = drive ? deflection: 0;
+            speed = drive ? deflection : 0;
         } else {
             // No new input: hold the previous target angle.
             desiredAngle = swerveModule1.getTargetAngle();
@@ -127,6 +176,93 @@ public class TriSwerve implements Subsystem {
         swerveModule2.setDesiredState(desiredAngle, speed);
         swerveModule3.setDesiredState(desiredAngle, speed);
     }
+
+    public int quickTripStage = 0;
+    public long quickTripTimer = 0;
+
+    public boolean quickTrip() {
+        switch (quickTripStage) {
+            case 0:
+                processDriverInput(0, 1, true);
+                if (localizer.getPose().position.x > quickTripDistance) {
+                    processDriverInput((localizer.getPose().position.y / Math.abs(localizer.getPose().position.y)) * 1, 0, false);
+                    quickTripStage++;
+                    quickTripTimer = futureTime(.5);
+                }
+                break;
+
+            case 1:
+                processDriverInput((localizer.getPose().position.y / Math.abs(localizer.getPose().position.y)) * 1, 0, false);
+                if (isPast(quickTripTimer)) {
+                    quickTripStage++;
+                }
+
+                break;
+            case 2:
+                processDriverInput((localizer.getPose().position.y / Math.abs(localizer.getPose().position.y)) * 1, 0, true);
+
+                if (withinError(localizer.getPose().position.y, 0, 5)) {
+                    processDriverInput(0, 0, false);
+                    quickTripTimer = futureTime(2);
+                    processDriverInput(0, -1, false);
+                    quickTripStage++;
+                }
+                break;
+            case 3:
+                processDriverInput(0, -1, false);
+                if (isPast(quickTripTimer)) {
+                    quickTripStage++;
+                }
+
+                break;
+
+            case 4:
+                processDriverInput(0, -1, true);
+
+                if (localizer.getPose().position.x < 0) {
+                    processDriverInput((localizer.getPose().position.y / Math.abs(localizer.getPose().position.y)) * 1, 0, false);
+                    quickTripStage++;
+                    quickTripTimer = futureTime(1);
+                }
+                break;
+            case 5:
+                processDriverInput((localizer.getPose().position.y / Math.abs(localizer.getPose().position.y)) * 1, 0, true);
+                if (withinError(localizer.getPose().position.y, 0, 5)) {
+                    processDriverInput(0, 0, false);
+                    return true;
+                }
+                break;
+        }
+        return false;
+    }
+
+    int fourCornersStage = 0;
+
+    public boolean fourCorners() {
+        switch (fourCornersStage) {
+            case 1:
+
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
+
+        return false;
+    }
+
+    public double getCanTx() {
+        limelight.pipelineSwitch(6);
+        LLResult llResult;
+//                break;
+//            case 1:
+        if ((llResult = limelight.getLatestResult()) != null) {
+            return llResult.getTx();
+        }
+        else return -256;
+    }
+
     @Override
     public void update(Canvas fieldOverlay) {
         // Update chassis heading from the IMU.
@@ -137,12 +273,19 @@ public class TriSwerve implements Subsystem {
         swerveModule1.update();
         swerveModule2.update();
         swerveModule3.update();
+        localizer.update();
+        if(gripperReady) {
+            gripper.setPosition(Utils.servoNormalize(gripperOpen ? gripperOpenPosition : gripperClosedPosition));
+        }
+        else
+            gripper.setPosition(gripperStartPosition);
 
         // Draw chassis/robot representation on the dashboard.
         drawRobot(fieldOverlay);
     }
 
-    public void initIMU(){
+
+    public void initIMU() {
         imu = hardwareMap.get(BNO055IMU.class, "imu");
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
         parameters.mode = BNO055IMU.SensorMode.IMU;
