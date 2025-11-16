7d5be4a071d3519b3dd8b534f246c34a84c3542d
TheOnlyNightingale <nirjharahul@gmail.com>
2025-11-15 18:28:21 -0600

    pid tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/lebot/PIDConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/lebot/PIDConstants.java
@@ -4,7 +4,7 @@ import com.acmerobotics.dashboard.config.Config;
 
 @Config
 public class PIDConstants {
-    public static double Kp = 1.0;
+    public static double Kp = 2.0;
     public static double Ki = 0.0;
-    public static double Kd = 0.031;
+    public static double Kd = 0.0;
 }
