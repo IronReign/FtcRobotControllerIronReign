b07e91306e1c89a713ff9a343cb6111e6533a278
IronReignRobotics <ironreignrobotics@gmail.com>
2026-02-01 12:17:43 -0600

    Fix Pinpoint Y pod offset: reverse encoder direction to match positive offset convention
    Y pod was configured FORWARD with positive offset (+131.9mm), but the
    Pinpoint firmware expects the opposite sign convention. Verified in
    sample OpMode that REVERSED + positive offset produces correct rotation
    compensation. Eliminates ~14.5" phantom translation per 90 degree turn.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/lebot2/rr_localize/PinpointLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/lebot2/rr_localize/PinpointLocalizer.java
@@ -57,7 +57,7 @@ public final class PinpointLocalizer implements Localizer {
         //driver.setOffsets(mmPerTick * PARAMS.parYTicks, mmPerTick * PARAMS.perpXTicks, DistanceUnit.MM);
         // TODO: reverse encoder directions if needed
         initialParDirection = GoBildaPinpointDriver.EncoderDirection.FORWARD;
-        initialPerpDirection = GoBildaPinpointDriver.EncoderDirection.FORWARD;
+        initialPerpDirection = GoBildaPinpointDriver.EncoderDirection.REVERSED;
 
         driver.setEncoderDirections(initialParDirection, initialPerpDirection);
 
