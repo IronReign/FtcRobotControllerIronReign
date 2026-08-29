5ce21e4270b5b425b0a2e7a335d72a4611aba46a
averageatbest <octaviogamez466@gmail.com>
2026-08-29 14:46:02 -0500

    feat: finish robot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/newBot/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/newBot/Robot.java
@@ -2,14 +2,52 @@ package org.firstinspires.ftc.teamcode.robots.newBot;
 
 public class Robot implements TelemetryProvider {
 
-    // SUBSYSTEM INIT
-    public final DriveTrainBase driveTrain;
+    // SUBSYSTEMS DECLARATION
+    private final DriveTrainBase driveTrain;
+
+    //SUBSYTEM ARRAY DECLARATION
+    private final List<Subsystem> subsystems = new ArrayList<>();
+
+
 
     public Robot(HardwareMap hardwareMap)
     {
+        // SBUSYSTEM INIT
         driveTrain = new MeccanumDrive(hardwareMap);
+        
+        // ADD SUBSYSTEMS TO ARRAY
+        subsystems.add(driveTrain);
+        
     }
 
+
+    public void update(){
+        
+        // I2C SENSOR READ 
+        for(Subsystem subsystem : subsystems)
+            subsystem.readSensors();
+
+        // RUN CALCULATIONS
+        for (Subsystem subsystem : subsystems) 
+            subsystem.calc(fieldOverlay);
+        
+        //FLUSH PENDING COMMANDS
+        for (Subsystem subsystem : subsystems) 
+            subsystem.act();
+        
+    }
+
+
+    public void stop()
+    {
+        for(Subsystem subsystem : subsystems)
+            subsystem.stop();
+    }
+
+    public void resetStates({
+        for(Subsystem subsystem : subsystems)
+            subsystem.resetStates();
+    }
     
 
 }
