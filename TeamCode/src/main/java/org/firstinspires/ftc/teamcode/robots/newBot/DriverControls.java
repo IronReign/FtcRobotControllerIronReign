39f86b8f9e7a7896de023e724499ede87a4868e5
averageatbest <octaviogamez466@gmail.com>
2026-08-29 12:05:31 -0500

    feat: implement DriverControls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/newBot/DriverControls.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/newBot/DriverControls.java
@@ -1,11 +1,22 @@
 package org.firstinspires.ftc.teamcode.robots.newBot;
 
+@Config(value = "newBot_DriverControls")
 public class DriverControls {
 
+    private final Gamepad gamepad1;
+    private final NewBot robot;
 
-    
+    public DriverControls( Gamepad gamepad1, NewBot robot)
+    {
+        this.gamepad1 = gamepad1;
+        this.robot = robot;
+    }
 
-    public void joystickDrive() {
+    public void update(){
+        handleJoystickDrive();
+    }
+
+    public void handleJoystickDrive() {
 
         double forward = -gamepad1.left_stick_y;
         double strafe = gamepad1.left_stick_x;
