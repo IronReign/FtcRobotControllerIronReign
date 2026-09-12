b53bad1212697faa372a1c332f4a94d91abf46c3
averageatbest <octaviogamez466@gmail.com>
2026-09-12 13:41:39 -0500

    feat: add teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/newBot/NewBotTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/newBot/NewBotTeleOp.java
@@ -1,4 +1,32 @@
 package org.firstinspires.ftc.teamcode.robots.newBot;
 
-public class NewBotTeleOp {
+import com.acmerobotics.dashboard.canvas.Canvas;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "NewBotTeleOp", group = "newBot")
+public class NewBotTeleOp extends OpMode {
+    private Robot robot;
+    private DriverControls driverControls;
+
+    @Override
+    public void init() {
+        robot = new Robot(hardwareMap);
+        robot.stop();
+        driverControls = new DriverControls(gamepad1, robot);
+
+    }
+
+    @Override
+    public void loop() {
+        driverControls.update();
+        robot.update(new Canvas());
+    }
+
+    @Override
+    public void stop() {
+        if (robot != null) {
+            robot.stop();
+        }
+    }
 }
