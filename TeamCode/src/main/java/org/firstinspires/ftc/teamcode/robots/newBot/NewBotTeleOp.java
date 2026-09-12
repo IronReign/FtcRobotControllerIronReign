30892ff1e12e7c05cd8bb89a21155276f11db89e
averageatbest <octaviogamez466@gmail.com>
2026-09-12 14:37:39 -0500

    feat: add flywhell subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/newBot/NewBotTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/newBot/NewBotTeleOp.java
@@ -4,6 +4,8 @@ import com.acmerobotics.dashboard.canvas.Canvas;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import java.util.Map;
+
 @TeleOp(name = "NewBotTeleOp", group = "newBot")
 public class NewBotTeleOp extends OpMode {
     private Robot robot;
@@ -15,12 +17,18 @@ public class NewBotTeleOp extends OpMode {
         robot.stop();
         driverControls = new DriverControls(gamepad1, robot);
 
+        telemetry.addLine("Left stick: drive/strafe. Right stick: turn.");
+        telemetry.addLine("A: flywheel on/off. RB: intake. LB: reverse intake.");
+        telemetry.addData("Flywheel mode", robot.flywheel.getTelemetry(false).get("Mode"));
     }
 
     @Override
     public void loop() {
         driverControls.update();
         robot.update(new Canvas());
+        for (Map.Entry<String, Object> entry : robot.flywheel.getTelemetry(false).entrySet()) {
+            telemetry.addData(entry.getKey(), entry.getValue());
+        }
     }
 
     @Override
